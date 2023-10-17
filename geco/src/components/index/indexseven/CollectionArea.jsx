63feed6b60ba5628bfa4e790b254dd0e29600b70
Saradhi <saradhi0001069@gmail.com>
2023-10-17 10:36:25 +0530

    added new images

--- geco/src/components/index/indexseven/CollectionArea.jsx
+++ geco/src/components/index/indexseven/CollectionArea.jsx
@@ -8,24 +8,24 @@ const CollectionArea = () => {
             <div className="col-lg-6 col-md-9">
               <div className="latest-collection-item mb-30">
                 <div className="lc-item-thumb">
-                  <a href="/#"><img src="assets/img/product/latest_collection01.jpg" alt="" /></a>
+                  <a href="/#"><img src="assets/img/product/latest_games_thumb01.jpg" alt="" /></a>
                 </div>
                 <div className="lc-item-content">
                   <h4>Latest Collection</h4>
                   <p>Lorem ipsum dolor sitjst amet consetur adipisng</p>
-                  <a href="/#" className="btn rotated-btn">Sell Now</a>
+                  <a href="/#" className="btn rotated-btn">Buy Now</a>
                 </div>
               </div>
             </div>
             <div className="col-lg-6 col-md-9">
               <div className="latest-collection-item mb-30">
                 <div className="lc-item-thumb">
-                  <a href="/#"><img src="assets/img/product/latest_collection02.jpg" alt="" /></a>
+                  <a href="/#"><img src="assets/img/product/latest_games_thumb02.jpg" alt="" /></a>
                 </div>
                 <div className="lc-item-content">
                   <h4>Best Selling Items</h4>
                   <p>Lorem ipsum dolor sitjst amet consetur adipisng</p>
-                  <a href="/#" className="btn rotated-btn">buy Now</a>
+                  <a href="/#" className="btn rotated-btn">Sell Now</a>
                 </div>
               </div>
             </div>
